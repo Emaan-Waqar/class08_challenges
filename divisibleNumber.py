9ed477875b914c4da56f9de1eae8d87b2c75f3c7
Emaan Waqar <emaanwaqar0102@gmail.com>
2025-07-23 13:01:54 +0500

    Divisiblity checker

---
+++ divisibleNumber.py
@@ -0,0 +1,7 @@
+numN= int(input("Enter the numerator: "))
+numD= int(input("Enter the denominator: "))
+
+if numN%numD==0:
+    print("\n" +str(numN)+ " is divisible by " +str(numD))
+else:
+    print("\n" +str(numN)+ " is not divisble " +str(numD))    
